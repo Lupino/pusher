b62e2e8637b0fd783e04994f2898141d58974e62
Lupino <lmjubuntu@gmail.com>
2016-02-22 09:55:11 +0800

    Add pusher worker

---
+++ worker.go
@@ -0,0 +1,29 @@
+package pusher
+
+import (
+	"github.com/Lupino/go-periodic"
+)
+
+var periodicWorker *periodic.Worker
+
+func warperPlugin(plugin Plugin) func(periodic.Job) {
+	return func(job periodic.Job) {
+		later, err := plugin.Do(job.Name, job.Args)
+
+		if err != nil {
+			job.Fail()
+		} else if later > 0 {
+			job.SchedLater(later)
+		} else {
+			job.Done()
+		}
+	}
+}
+
+// NewWorker defined new pusher worker
+func NewWorker(w *periodic.Worker, plugins []Plugin) {
+	for _, plugin := range plugins {
+		w.AddFunc(plugin.GetGroupName(), warperPlugin(plugin))
+	}
+	w.Work()
+}
